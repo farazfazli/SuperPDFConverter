890f18987f4cc19f6a7b2c68886193c0effb92fc
Faraz <farazfazli@gmail.com>
2015-07-18 17:54:41 -0400

    Changed File Browser to a simpler one, added new Download Status layout. Added logo. Code cleanup.

--- app/src/main/java/com/farazfazli/superpdfconverter/MainActivity.java
+++ app/src/main/java/com/farazfazli/superpdfconverter/MainActivity.java
@@ -1,19 +1,15 @@
 package com.farazfazli.superpdfconverter;
 
+import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.ActivityNotFoundException;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,26 +17,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.File;
-import java.net.MalformedURLException;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLEncoder;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+    private static final String convertToPDF = "http://www.FreeHTMLtoPDF.com/?convert=";
+    private static final String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/SuperPDFConverter/";
+    public String url;
+    public File directoryPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/SuperPDFConverter/");
     private EditText urlField;
     private Button convertToPDFButton;
     private Button viewHistory;
     private Button deleteHistory;
     private TextView history;
     private String website;
-    private String url;
     private URL urlToConvert;
     private Uri uriToConvert;
     private DownloadManager manager;
     private long downloadId;
-    private static final String convertToPDF = "http://www.FreeHTMLtoPDF.com/?convert=";
-    private static final String path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/SuperPDFConverter/";
-    public File directoryPath = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/SuperPDFConverter/");
     private int count = 0;
+
+    public static boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                } else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +59,22 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         urlField = (EditText) findViewById(R.id.urlField);
         history = (TextView) findViewById(R.id.history);
-
         convertToPDFButton = (Button) findViewById(R.id.grabPdf);
         convertToPDFButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 website = urlField.getText().toString();
-                url = convertToPDF + website;
-                convertToPDF(url);
+                try {
+                    url = convertToPDF + URLEncoder.encode(website, "UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    Toast.makeText(MainActivity.this, "Must be a valid URL!", Toast.LENGTH_SHORT).show();
+                }
+                if (website.length() > 3 && Patterns.WEB_URL.matcher(website).matches()) {
+                    convertToPDF(url);
+                } else {
+
+                    Toast.makeText(MainActivity.this, "Must contain more than 3 characters be a valid URL!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -63,7 +82,7 @@ public class MainActivity extends ActionBarActivity {
         viewHistory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FileChooser.class);
+                Intent intent = new Intent(MainActivity.this, AndroidFileBrowser.class);
                 startActivity(intent);
             }
         });
@@ -100,113 +119,94 @@ public class MainActivity extends ActionBarActivity {
                 alert.show();
             }
         });
-        getFile();
+        updateNumberOfFiles();
     }
-    public void convertToPDF(String url) {
-            convertToPDFButton.setText("Wait!");
-            convertToPDFButton.setClickable(false);
-            convertToPDFButton.setAlpha(.5f);
-            try {
-                urlToConvert = new URL(url);
-                uriToConvert = Uri.parse(url);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-            if (uriToConvert != null) {
-                DownloadManager.Request request = new DownloadManager.Request(uriToConvert);
-                request.setTitle(website + ".pdf");
-                request.setDescription("PDF for: " + website);
-                request.setMimeType("application/pdf");
-// in order for this if to run, you must use the android 3.2 to compile your app
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-                    request.allowScanningByMediaScanner();
-                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-                }
-                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS + "/SuperPDFConverter/", website + ".pdf");
-// get download service and enqueue file
-                manager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
-                downloadId = manager.enqueue(request);
-                try {
-                    urlToConvert = new URL(url);
-                    uriToConvert = Uri.parse(url);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-                PackageManager packageManager = this.getPackageManager();
-                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
 
-                BroadcastReceiver onComplete = new BroadcastReceiver() {
-                    public void onReceive(Context ctxt, Intent intent) {
-                        getFile();
-                        openPDF();
-                        convertToPDFButton.setText("Grab PDF!");
-                        convertToPDFButton.setClickable(true);
-                        convertToPDFButton.setAlpha(1f);
-                    }
-                };
-                registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-                MainActivity mainActivity = new MainActivity();
-            }
-        }
-
-        public void openPDF() {
-            getFile();
-            File pdf = new File(path + website + ".pdf");
-            if (pdf.exists()) {
-                Uri path = Uri.fromFile(pdf);
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_VIEW);
-                intent.setDataAndType(path, "application/pdf");
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                try {
-                    startActivity(intent);
-                } catch (ActivityNotFoundException e) {
-                    Toast.makeText(MainActivity.this,
-                            "No application available to view PDF!",
-                            Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-
-    public void getFile() {
+    public void updateNumberOfFiles() {
         File f = new File(path);
         File[] files = f.listFiles();
         count = 0;
         if (files != null) {
-            for (int i = 0; i < files.length; i++) {
+            for (File file : files) {
                 count++;
-//                File file = files[i];
-
-//                if (file.isDirectory()) {
-//                    getFile(file.getAbsolutePath());
-//                }
             }
 
             if (count != 0){
                 if (count == 1){
-                    history.setText(count + " PDF!\nPress View History to view it!");
+                    history.setText(count + " PDF converted ran!\nPress View History to view it!");
                 } else if(count > 1){
-                    history.setText(count + " PDFs\nPress View History to view them!");
+                    history.setText(count + " PDFs Converted!\nPress View History to view them!");
                 }
             }
         }
         else {
-            history.setText("No PDFs saved!");
+            history.setText("No history available!");
         }
     }
-    public static boolean deleteDirectory(File path) {
-        if( path.exists() ) {
-            File[] files = path.listFiles();
-            for(int i=0; i<files.length; i++) {
-                if(files[i].isDirectory()) {
-                    deleteDirectory(files[i]);
-                }
-                else {
-                    files[i].delete();
+
+    public void convertToPDF(String url) {
+        Intent intent = new Intent(this, PDFManager.class);
+        intent.putExtra("WEBSITE_NAME", website);
+        intent.putExtra("url", url);
+        startActivity(intent);
+    }
+
+    public void setPath(String filePath) {
+        history.setText("Path to file: " + filePath);
+    }
+
+    public void openFile(String pdfFilename) {
+        File pdf = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath() + "/SuperPDFConverter/" + pdfFilename);
+        if (pdf.exists()) {
+            setPath(pdf.getPath());
+            Uri path = Uri.fromFile(pdf);
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setDataAndType(path, "application/pdf");
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            try {
+                startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(MainActivity.this,
+                        "No application available to view PDF!",
+                        Toast.LENGTH_SHORT).show();
+/*                Intent intent = new Intent(Intent.ACTION_VIEW)
+                        .setData(Uri.parse("http://docs.google.com/gview?embedded=true&url=" + url));
+                startActivity(intent);
+            }
+*/
+            }
+        } else {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Error!")
+                    .setMessage("PDF not found! Please try again!")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateNumberOfFiles();
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            if (extras.containsKey("pdf_filename")) {
+                String pdfFilename = extras.getString("pdf_filename");
+                extras.remove("pdf_filename");
+                if (!pdfFilename.isEmpty()) {
+                    openFile(pdfFilename);
+                    getIntent().removeExtra("pdf_filename");
+                    pdfFilename = "";
                 }
             }
         }
-        return(path.delete());
     }
 
 /*    @Override
